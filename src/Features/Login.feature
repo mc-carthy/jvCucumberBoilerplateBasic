e6d70d1f3b26e1e1a602f94b2502aedde171b2ff
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-12 15:52:27 +0000

    Create basic feature and empty stepDefs

---
+++ src/Features/Login.feature
@@ -0,0 +1,8 @@
+Feature: Login Feature
+  This feature deals with the login functionality of the application
+
+  Scenario: Login with valid credentials
+    Given I navigate to the login page
+    When I enter the username as admin and password as admin
+    And I click the login button
+    Then I should the userform page
---
+++ src/test/java/LoginStep.java
@@ -0,0 +1,32 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStep {
+
+    @Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I enter the username as admin and password as admin$")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click the login button$")
+    public void iClickTheLoginButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should the userform page$")
+    public void iShouldTheUserformPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
